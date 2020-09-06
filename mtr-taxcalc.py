ebc367323e580b993ebc5634814a6b0b946c4e9a
Cody Kallen <cody_kallen@yahoo.com>
2020-09-06 13:21:31 -0500

    More work and tabulations

---
+++ mtr-taxcalc.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  6 07:36:10 2020
+
+@author: cody_
+"""
+
+import os
+os.chdir('C:/Users/cody_/Documents/GitHub/coc-fl')
+import numpy as np
+import pandas as pd
+import taxcalc
+from taxcalc import *
+
+puf_path = 'puf.csv'
+
+def make_calculator(refdict=None, year=2020):
+    """
+    Creates a calculator advanced to the given year and calculates tax results
+    Note: Passing an empty dictionary to refdict produces a 
+          current law calculator (TCJA law)
+    """
+    assert year in range(2014, 2030)
+    pol = Policy()
+    rec = Records(puf_path)
+    if refdict is not None:
+        assert type(refdict) is dict
+        pol.implement_reform(refdict)
+    calc1 = Calculator(policy=pol, records=rec, verbose=False)
+    calc1.advance_to_year(year)
+    calc1.calc_all()
+    return calc1
+
+def calcTauNC(calc):
+    """
+    Calculates the effective marginal tax rate on each type of pass-through business
+    income.
+    """
+    inc1 = np.abs(calc.array('e00900'))
+    inc2 = np.abs(calc.array('e26270'))
+    inc3 = np.abs(calc.array('e02000') - calc.array('e26270'))
+    wgt = calc.array('s006')
+    mtr1 = calc.mtr('e00900p', calc_all_already_called=True)[2]
+    mtr2 = calc.mtr('e26270', calc_all_already_called=True)[2]
+    mtr3 = calc.mtr('e02000', calc_all_already_called=True)[2]
+    wmtr1 = sum(mtr1 * inc1 * wgt) / sum(inc1 * wgt)
+    wmtr2 = sum((mtr2 * inc2 + mtr3 * inc3) * wgt) / sum((inc2 + inc3) * wgt)
+    return (wmtr1, wmtr2)
+
+
+# Create baseline
+calc_base = make_calculator(None, 2019)
+
+# Version with extension of II tax policies
+param_extII = Calculator.read_json_param_objects('JSONs/extendII.json', None)
+calc_extII = make_calculator(param_extII['policy'], 2019)
+
+# Version with Biden plan
+#param_biden = Calculator.read_json_param_objects('JSONs/biden.json', None)
+#calc_biden = make_calculator(param_biden['policy'], 2019)
+
+# Compute relevant MTRs for each year
+mtr_sp_base = np.zeros(10)
+mtr_ps_base = np.zeros(10)
+mtr_sp_extII = np.zeros(10)
+mtr_ps_extII = np.zeros(10)
+#mtr_sp_biden = np.zeros(10)
+#mtr_ps_biden = np.zeros(10)
+
+for i in range(10):
+    # Advance each Calculator
+    calc_base.increment_year()
+    calc_base.calc_all()
+    calc_extII.increment_year()
+    calc_extII.calc_all()
+    #calc_biden.increment_year()
+    #calc_biden.calc_all()
+    # Compute and save MTRs
+    mtrs1 = calcTauNC(calc_base)
+    mtr_sp_base[i] = mtrs1[0]
+    mtr_ps_base[i] = mtrs1[1]
+    mtrs2 = calcTauNC(calc_extII)
+    mtr_sp_extII[i] = mtrs2[0]
+    mtr_ps_extII[i] = mtrs2[1]
+#    (mtr_sp_biden(i), mtr_ps_biden(i)) = calcTauNC(calc_biden)
+
+# Update policy CSVs with MTRs
+pols_base = pd.read_csv('policy_baseline.csv')
+pols_extII = pd.read_csv('policy_extendII.csv')
+pols_extAll = pd.read_csv('policy_extendAll.csv')
+#pols_biden = pd.read_csv('policy_biden.csv')
+
+pols_base['taxrt_scorp'] = mtr_ps_base
+pols_base['taxrt_soleprop'] = mtr_sp_base
+pols_base['taxrt_partner'] = mtr_ps_base
+pols_base.to_csv('policy_baseline.csv')
+
+pols_extII['taxrt_scorp'] = mtr_ps_extII
+pols_extII['taxrt_soleprop'] = mtr_sp_extII
+pols_extII['taxrt_partner'] = mtr_ps_extII
+pols_extII.to_csv('policy_extendII.csv')
+
+pols_extAll['taxrt_scorp'] = mtr_ps_extII
+pols_extAll['taxrt_soleprop'] = mtr_sp_extII
+pols_extAll['taxrt_partner'] = mtr_ps_extII
+pols_extAll.to_csv('policy_extendAll.csv')
+
+#pols_biden['taxrt_scorp'] = mtr_ps_biden
+#pols_biden['taxrt_soleprop'] = mtr_sp_biden
+#pols_biden['taxrt_partner'] = mtr_ps_biden
+#pols_biden.to_csv('policy_biden.csv')
+
+
--- outputBuilder.py
+++ outputBuilder.py
@@ -25,15 +25,16 @@ class OutputBuilder():
         # Read in asset data
         self.read_assets()
         assert type(key) is str
+        self.key = key
         
     def read_assets(self):
         """
         Read in asset data by asset type, industry and firm type.
         """
-        self.stock_ccorp = pd.read_csv(OUTPUTPATH + 'stock_ccorp.csv', index_col='asset')
-        self.stock_scorp = pd.read_csv(OUTPUTPATH + 'stock_scorp.csv', index_col='asset')
-        self.stock_soleprop = pd.read_csv(OUTPUTPATH + 'stock_soleprop.csv', index_col='asset')
-        self.stock_partner = pd.read_csv(OUTPUTPATH + 'stock_partner.csv', index_col='asset')
+        self.stock_ccorp = pd.read_csv(OUTPUTPATH + 'stock_ccorp.csv', index_col='asset').fillna(value = 0)
+        self.stock_scorp = pd.read_csv(OUTPUTPATH + 'stock_scorp.csv', index_col='asset').fillna(value = 0)
+        self.stock_soleprop = pd.read_csv(OUTPUTPATH + 'stock_soleprop.csv', index_col='asset').fillna(value = 0)
+        self.stock_partner = pd.read_csv(OUTPUTPATH + 'stock_partner.csv', index_col='asset').fillna(value = 0)
     
     def store_raw(self, year):
         """
@@ -58,4 +59,135 @@ class OutputBuilder():
         df_scorp2.to_csv(OUTPUTPATH + 'metr_scorp_' + self.key + '_' + str(year) + '.csv')
         df_soleprop2.to_csv(OUTPUTPATH + 'metr_soleprop_' + self.key + '_' + str(year) + '.csv')
         df_partner2.to_csv(OUTPUTPATH + 'metr_partner_' + self.key + '_' + str(year) + '.csv')
-        
+    
+    def tabulate_industry(self, year):
+        """
+        Compute weighted average cost of capital and METR by industry
+        for the given year.
+        """
+        return None
+    
+    def tabulate_asset(self, year):
+        """
+        Compute weighted average cost of capital and METR by asset type
+        for the given year.
+        """
+        return None
+    
+    def tabulate_firm(self, year):
+        """
+        Compute weighted average cost of capital and METR by firm type
+        for the given year.
+        """
+        return None
+    
+    def tabulate_main(self, year):
+        """
+        Compute weighted average cost of capital and METR for:
+            All
+            C corporations
+            S corporations
+            Sole proprietorships
+            Partnerships
+            Equipment
+            Structures
+            Rental residential
+            Intellectual property
+        """
+        assert str(year) in list(self.calc.results_coc)
+        # Extract asset weights
+        stock_ccorp_arr = self.stock_ccorp.to_numpy()
+        stock_scorp_arr = self.stock_scorp.to_numpy()
+        stock_soleprop_arr = self.stock_soleprop.to_numpy()
+        stock_partner_arr = self.stock_partner.to_numpy()
+        # Extract results by firm type
+        coc_ccorp = self.calc.results_coc[str(year)]['corp']
+        coc_scorp = self.calc.results_coc[str(year)]['scorp']
+        coc_soleprop = self.calc.results_coc[str(year)]['soleprop']
+        coc_partner = self.calc.results_coc[str(year)]['partner']
+        mtr_ccorp = self.calc.results_metr[str(year)]['corp']
+        mtr_scorp = self.calc.results_metr[str(year)]['scorp']
+        mtr_soleprop = self.calc.results_metr[str(year)]['soleprop']
+        mtr_partner = self.calc.results_metr[str(year)]['partner']
+        # Arrays for storing results
+        catlist = ['All', 'C corporations', 'S corporations',
+                   'Sole proprietorships', 'Partnerships',
+                   'Equipment', 'Structures', 'Residential',
+                   'Intellectual property']
+        coclist = np.zeros(9)
+        mtrlist = np.zeros(9)
+        # Store average results for cost of capital
+        coclist[0] = ((coc_ccorp * stock_ccorp_arr
+                       + coc_scorp * stock_scorp_arr
+                       + coc_soleprop * stock_soleprop_arr
+                       + coc_partner * stock_partner_arr).sum()
+                      / (stock_ccorp_arr + stock_scorp_arr
+                         + stock_soleprop_arr + stock_partner_arr).sum())
+        coclist[1] = (coc_ccorp * stock_ccorp_arr).sum() / stock_ccorp_arr.sum()
+        coclist[2] = (coc_scorp * stock_scorp_arr).sum() / stock_scorp_arr.sum()
+        coclist[3] = (coc_soleprop * stock_soleprop_arr).sum() / stock_soleprop_arr.sum()
+        coclist[4] = (coc_partner * stock_partner_arr).sum() / stock_partner_arr.sum()
+        coclist[5] = ((coc_ccorp[0:37,:] * stock_ccorp_arr[0:37,:]
+                       + coc_scorp[0:37,:] * stock_scorp_arr[0:37,:]
+                       + coc_soleprop[0:37,:] * stock_soleprop_arr[0:37,:]
+                       + coc_partner[0:37,:] * stock_partner_arr[0:37,:]).sum()
+                      / (stock_ccorp_arr[0:37,:] + stock_scorp_arr[0:37,:]
+                         + stock_soleprop_arr[0:37,:] + stock_partner_arr[0:37,:]).sum())
+        coclist[6] = ((coc_ccorp[37:68,:] * stock_ccorp_arr[37:68,:]
+                       + coc_scorp[37:68,:] * stock_scorp_arr[37:68,:]
+                       + coc_soleprop[37:68,:] * stock_soleprop_arr[37:68,:]
+                       + coc_partner[37:68,:] * stock_partner_arr[37:68,:]).sum()
+                      / (stock_ccorp_arr[37:68] + stock_scorp_arr[37:68,:]
+                         + stock_soleprop_arr[37:68,:] + stock_partner_arr[37:68,:]).sum())
+        coclist[7] = ((coc_ccorp[91,:] * stock_ccorp_arr[91,:]
+                       + coc_scorp[91,:] * stock_scorp_arr[91,:]
+                       + coc_soleprop[91,:] * stock_soleprop_arr[91,:]
+                       + coc_partner[91,:] * stock_partner_arr[91,:]).sum()
+                      / (stock_ccorp_arr[91,:] + stock_scorp_arr[91,:]
+                         + stock_soleprop_arr[91,:] + stock_partner_arr[91,:]).sum())
+        coclist[8] = ((coc_ccorp[68:91,:] * stock_ccorp_arr[68:91,:]
+                       + coc_scorp[68:91,:] * stock_scorp_arr[68:91,:]
+                       + coc_soleprop[68:91,:] * stock_soleprop_arr[68:91,:]
+                       + coc_partner[68:91,:] * stock_partner_arr[68:91,:]).sum()
+                      / (stock_ccorp_arr[68:91,:] + stock_scorp_arr[68:91,:]
+                         + stock_soleprop_arr[68:91,:] + stock_partner_arr[68:91,:]).sum())
+        # Store average results for METR
+        mtrlist[0] = ((mtr_ccorp * stock_ccorp_arr
+                       + mtr_scorp * stock_scorp_arr
+                       + mtr_soleprop * stock_soleprop_arr
+                       + mtr_partner * stock_partner_arr).sum()
+                      / (stock_ccorp_arr + stock_scorp_arr
+                         + stock_soleprop_arr + stock_partner_arr).sum())
+        mtrlist[1] = (mtr_ccorp * stock_ccorp_arr).sum() / stock_ccorp_arr.sum()
+        mtrlist[2] = (mtr_scorp * stock_scorp_arr).sum() / stock_scorp_arr.sum()
+        mtrlist[3] = (mtr_soleprop * stock_soleprop_arr).sum() / stock_soleprop_arr.sum()
+        mtrlist[4] = (mtr_partner * stock_partner_arr).sum() / stock_partner_arr.sum()
+        mtrlist[5] = ((mtr_ccorp[0:37,:] * stock_ccorp_arr[0:37,:]
+                       + mtr_scorp[0:37,:] * stock_scorp_arr[0:37,:]
+                       + mtr_soleprop[0:37,:] * stock_soleprop_arr[0:37,:]
+                       + mtr_partner[0:37,:] * stock_partner_arr[0:37,:]).sum()
+                      / (stock_ccorp_arr[0:37,:] + stock_scorp_arr[0:37,:]
+                         + stock_soleprop_arr[0:37,:] + stock_partner_arr[0:37,:]).sum())
+        mtrlist[6] = ((mtr_ccorp[37:68,:] * stock_ccorp_arr[37:68,:]
+                       + mtr_scorp[37:68,:] * stock_scorp_arr[37:68,:]
+                       + mtr_soleprop[37:68,:] * stock_soleprop_arr[37:68,:]
+                       + mtr_partner[37:68,:] * stock_partner_arr[37:68,:]).sum()
+                      / (stock_ccorp_arr[37:68] + stock_scorp_arr[37:68,:]
+                         + stock_soleprop_arr[37:68,:] + stock_partner_arr[37:68,:]).sum())
+        mtrlist[7] = ((mtr_ccorp[91,:] * stock_ccorp_arr[91,:]
+                       + mtr_scorp[91,:] * stock_scorp_arr[91,:]
+                       + mtr_soleprop[91,:] * stock_soleprop_arr[91,:]
+                       + mtr_partner[91,:] * stock_partner_arr[91,:]).sum()
+                      / (stock_ccorp_arr[91,:] + stock_scorp_arr[91,:]
+                         + stock_soleprop_arr[91,:] + stock_partner_arr[91,:]).sum())
+        mtrlist[8] = ((mtr_ccorp[68:91,:] * stock_ccorp_arr[68:91,:]
+                       + mtr_scorp[68:91,:] * stock_scorp_arr[68:91,:]
+                       + mtr_soleprop[68:91,:] * stock_soleprop_arr[68:91,:]
+                       + mtr_partner[68:91,:] * stock_partner_arr[68:91,:]).sum()
+                      / (stock_ccorp_arr[68:91,:] + stock_scorp_arr[68:91,:]
+                         + stock_soleprop_arr[68:91,:] + stock_partner_arr[68:91,:]).sum())
+        df1 = pd.DataFrame({'Category': catlist, 'CoC': coclist, 'METR': mtrlist})
+        df1.to_csv(OUTPUTPATH + 'main_' + self.key + '_' + str(year) + '.csv', index=False)
+
+
+
